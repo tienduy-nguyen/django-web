c90b44ec67fb6c5ebf72fc0eafced10b2f27eb9f
Tien Duy NGUYEN <39387827+tienduy-nguyen@users.noreply.github.com>
2020-05-31 19:23:18 +0200

    Migrate postgresql

--- real-estate/pages/urls.py
+++ real-estate/pages/urls.py
@@ -4,5 +4,8 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
-    path('about', views.about, name='about')
+    path('about', views.about, name='about'),
+    path('login', views.login, name='login'),
+    path('register', views.register, name='register'),
+
 ]
--- real-estate/pages/views.py
+++ real-estate/pages/views.py
@@ -10,3 +10,11 @@ def index(request):
 
 def about(request):
     return render(request, 'pages/about.html')
+
+
+def login(request):
+    return render(request, 'auth/login.html')
+
+
+def register(request):
+    return render(request, 'auth/register.html')
