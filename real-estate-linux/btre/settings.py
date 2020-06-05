b9d8504a6ddbee398d34542c76d2c793933febcb
tienduy-nguyen <tienduy.nguyen.dev@gmail.com>
2020-06-05 23:57:21 +0200

    Send mail

--- real-estate-linux/btre/settings.py
+++ real-estate-linux/btre/settings.py
@@ -138,3 +138,11 @@ MEDIA_URL = '/media/'
 MESSAGE_TAGS = {
     messages.ERROR: 'danger',
 }
+
+
+# Email config
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_POST = 587
+EMAIL_HOST_USER = ''
+EMAIL_HOST_PASSWORD = ''
+EMAIL_USE_TLS = True
