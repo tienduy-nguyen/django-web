e8b4764659e0e8c9e84aade3b05441f1d3ba2917
tienduy-nguyen <tienduy.nguyen.dev@gmail.com>
2020-06-19 11:02:36 +0200

    Minor update

--- django_blog/django_blog/settings.py
+++ django_blog/django_blog/settings.py
@@ -32,7 +32,7 @@ SECRET_KEY = env('SECRET_KEY')
 DEBUG = env('DEBUG')
 
 # Add domain name here if we need deploy online
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -52,7 +52,7 @@ INSTALLED_APPS = [
     'markdown_deux',
     'markdownx',
     'multiselectfield',
-    'storages'
+    'storages',
 ]
 TAGGIT_CASE_INSENSITIVE = True
 
@@ -234,9 +234,9 @@ EMAIL_PORT = 587
 # Storage fo aws
 AWS_ACCESS_KEY_ID = os.environ.get('AWS_ACCESS_KEY_ID')
 AWS_SECRET_ACCESS_KEY = os.environ.get('AWS_SECRET_ACCESS_KEY')
-AWS_STORAGE_BUCKET_NAME = os.environ.get('AWS_STORAGE_BUCKET_NAME ')
+AWS_STORAGE_BUCKET_NAME = os.environ.get('AWS_STORAGE_BUCKET_NAME')
 
 AWS_S3_FILE_OVERWRITE = False
 AWS_DEFAULT_ACL = None
 DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
-STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+# STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
