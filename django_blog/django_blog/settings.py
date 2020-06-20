f68bab2285183bd46fc8fbde3b34bb40ea0d0f65
tienduy-nguyen <tienduy.nguyen.dev@gmail.com>
2020-06-20 12:53:42 +0200

    Use default database

--- django_blog/django_blog/settings.py
+++ django_blog/django_blog/settings.py
@@ -120,6 +120,8 @@ WSGI_APPLICATION = 'django_blog.wsgi.application'
 #         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
 #     }
 # }
+
+'''
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
@@ -130,6 +132,14 @@ DATABASES = {
         'PORT': env('DATABASE_PORT')
     }
 }
+'''
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    }
+}
+
 import dj_database_url
 db_from_env=dj_database_url.config(conn_max_age=600,ssl_require=True)
 DATABASES['default'].update(db_from_env)
